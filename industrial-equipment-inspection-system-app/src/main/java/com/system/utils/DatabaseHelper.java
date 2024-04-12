fd2fbcb3be1a7e403ca09ed0bd3fb299344eb0f7
tapaswi <satyapanthi.t@northeastern.edu>
2024-04-12 13:22:16 -0400

    added plant CRUD operations

--- industrial-equipment-inspection-system-app/src/main/java/com/system/utils/DatabaseHelper.java
+++ industrial-equipment-inspection-system-app/src/main/java/com/system/utils/DatabaseHelper.java
@@ -1,8 +1,12 @@
 package com.system.utils;
 
-import com.system.models.Equipment;
+import com.system.models.*;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DatabaseHelper {
 
@@ -50,6 +54,152 @@ public class DatabaseHelper {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    ResultSet fetchData(String query){
+        try {
+            return connection.createStatement().executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+        }
+
+        boolean executeDeleteQuery(String query, int id){
+            try {
+                PreparedStatement statement = connection.prepareStatement(query);
+                statement.setInt(1, id);
+                return statement.executeUpdate() > 0;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+    public List<EquipmentType> fetchEquipmentTypes(){
+        String query = "SELECT * FROM sql5694823.equipment_type;";
+        ResultSet rs = fetchData(query);
+        List<EquipmentType> equipmentTypes = new ArrayList<>();
+        try {
+            while(rs.next()){
+                equipmentTypes.add(new EquipmentType(rs.getInt("id"),
+                        rs.getString("name")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return equipmentTypes;
+    }
+
+    public List<Location> fetchLocations(){
+        String query = "SELECT * FROM sql5694823.location;";
+        ResultSet rs = fetchData(query);
+        List<Location> locations = new ArrayList<>();
+        try {
+            while(rs.next()){
+                locations.add(new Location(rs.getInt("id"),
+                        rs.getString("name")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return locations;
+    }
+
+    public List<Section> fetchSections(){
+        String query = "SELECT * FROM sql5694823.section;";
+        ResultSet rs = fetchData(query);
+        List<Section> sections = new ArrayList<>();
+        try {
+            while(rs.next()){
+                sections.add(new Section(rs.getInt("id"),
+                        rs.getString("name")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return sections;
+    }
+
+    public List<Floor> fetchFloors(){
+        String query = "SELECT * FROM sql5694823.floor;";
+        ResultSet rs = fetchData(query);
+        List<Floor> floors = new ArrayList<>();
+        try {
+            while(rs.next()){
+                floors.add(new Floor(rs.getInt("id"),
+                        rs.getString("name")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return floors;
+    }
+
+    public List<DataType> fetchTypes(){
+        String query = "SELECT * FROM sql5694823.data_type;";
+        ResultSet rs = fetchData(query);
+        List<DataType> dataTypes = new ArrayList<>();
+        try {
+            while(rs.next()){
+                dataTypes.add(new DataType(rs.getInt("id"),
+                        rs.getString("type")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return dataTypes;
+    }
+
+    public List<Plant> fetchPlants(){
+        String query = "SELECT * FROM sql5694823.plant;";
+        ResultSet rs = fetchData(query);
+        List<Plant> plants = new ArrayList<>();
+        try {
+            while(rs.next()){
+                plants.add(new Plant(rs.getInt("id"),
+                        rs.getString("Name")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return plants;
+    }
+
+    public boolean deletePlant(int id){
+        String query = "DELETE FROM sql5694823.plant where id = ?";
+        return executeDeleteQuery(query, id);
+    }
+    
+
+    public boolean updatePlant(Plant plant){
+        String query = "UPDATE sql5694823.plant SET Name = ? WHERE id = ?";
+        PreparedStatement statement;
+        try {
+            statement = connection.prepareStatement(query);
+            statement.setString(1, plant.getName());
+            statement.setInt(2, plant.getId());
+            return statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    
+    
+
+    public boolean insertPlant(Plant plant){
+        String query = "INSERT INTO sql5694823.plant (Name) VALUES (?)";
+        PreparedStatement statement;
+        try {
+            statement = connection.prepareStatement(query);
+            statement.setString(1, plant.getName());
+            return statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
