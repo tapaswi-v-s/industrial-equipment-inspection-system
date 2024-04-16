da0cc1acd3f6807d293d96ba030cdb4e3e71d1f0
HP <rachitas9598@gmail.com>
2024-04-16 02:27:31 -0400

    Added variables and getter setter methods for that.

--- industrial-equipment-inspection-system-app/src/main/java/com/system/models/Inspection.java
+++ industrial-equipment-inspection-system-app/src/main/java/com/system/models/Inspection.java
@@ -1,20 +1,86 @@
 package com.system.models;
 
+import java.util.Date;
+
 public class Inspection {
     private final Integer id;
-    private final String name;
+    private String inspection_date;
+    private String remark;
+    private Integer plant_Id;
+    private Integer inspector_id;
+    private Integer evaluator_id;
+    private String evaluator_remark;
 
-    public Inspection(Integer id, String name) {
+    
+       public Inspection(Integer id,String inspection_date,String remark,Integer plant_Id,
+               Integer inspector_id,Integer evaluator_id,String evaluator_remark) {
         this.id = id;
-        this.name = name;
+        this.inspection_date= inspection_date;
+        this.remark= remark;
+        this.plant_Id= plant_Id;
+        this.inspector_id= inspector_id;
+        this.evaluator_id= evaluator_id;
+        this.evaluator_remark=evaluator_remark;
     }
+       
+        public Inspection(Integer id) {
+        this.id = id;
+        
+    }
+    
+    public String getInspection_date() {
+        return inspection_date;
+    }
+
+    public void setInspection_date(String inspection_date) {
+        this.inspection_date = inspection_date;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public Integer getPlant_Id() {
+        return plant_Id;
+    }
+
+    public void setPlant_Id(Integer plant_Id) {
+        this.plant_Id = plant_Id;
+    }
+
+    public Integer getInspector_id() {
+        return inspector_id;
+    }
+
+    public void setInspector_id(Integer inspector_id) {
+        this.inspector_id = inspector_id;
+    }
+
+    public Integer getEvaluator_id() {
+        return evaluator_id;
+    }
+
+    public void setEvaluator_id(Integer evaluator_id) {
+        this.evaluator_id = evaluator_id;
+    }
+
+    public String getEvaluator_remark() {
+        return evaluator_remark;
+    }
+
+    public void setEvaluator_remark(String evaluator_remark) {
+        this.evaluator_remark = evaluator_remark;
+    }
+
 
     public Integer getId(){
         return id;
     }
 
-    public String getName(){
-        return name;
-    }
+   
 
 }
